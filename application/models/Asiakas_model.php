88502cfc39678331f2fe5e7f0e1c0984a5ac4718
Unknown <t4trjo00@students.oamk.fi>
2016-11-02 10:02:45 +0200

    asiakkaan lisäys toimii

--- application/models/Asiakas_model.php
+++ application/models/Asiakas_model.php
@@ -8,6 +8,15 @@ class Asiakas_model extends CI_Model
 		return $this->db->get()->result_array();
 
 	}
+	public function addAsiakas($lisaa_asiakas)
+	{
+		$this->db->set($lisaa_asiakas);
+		$this->db->insert('asiakas');
+		$testi=$this->db->affected_rows();
+		return $testi;
+
+
+	}
 
 }
 
--- application/views/asiakas/lisaa.php
+++ application/views/asiakas/lisaa.php
@@ -1,1 +1,8 @@
 <h1>Lisää asiakas</h1>
+<form method="POST" action="lisaa">
+<label>Etunimi</label><input type="text" name="en"> <br>
+<label>Sukunimi</label><input type="text" name="sn"> <br>
+<label>Email</label><input type="text" name="em"> <br>
+<input type="submit" name="btnTallenna" value="tallenna">
+
+</form>
